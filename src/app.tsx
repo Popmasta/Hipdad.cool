aff6fbd31ffb08e16eea08868a1f5ff38458c273
Spencer Rudnick <spencerudnick@gmail.com>
2019-02-24 17:52:47 -0600

    reactified

---
+++ src/app.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import './app.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <>
+        <header>
+          <h1>
+            Hip Dad Radio
+          </h1>
+        </header>
+        <main>
+          <h2>
+            Hello Hip Dads!
+          </h2>
+          <p>Stay tuned for updates...</p>
+        </main>
+        <footer>
+          <ul>
+            <li>more info here?</li>
+          </ul>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default App;
