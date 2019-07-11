64611dec1e60992aa38f57f071cf12c911127dba
wrudnick <rudnickw@msu.edu>
2019-07-11 13:10:45 -0500

    Reorganizing, react app works in GH pages but hipdad.cool will not route

---
+++ build/precache-manifest.115ea3a0ccb0e85d0f676b1a75b5749f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6ee0d8fef512bdeccd8ceee918c0c9",
+    "url": "/Hipdad.cool/index.html"
+  },
+  {
+    "revision": "a6fde463f8613648cb62",
+    "url": "/Hipdad.cool/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Hipdad.cool/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a6fde463f8613648cb62",
+    "url": "/Hipdad.cool/static/js/main.105da04d.chunk.js"
+  },
+  {
+    "revision": "bd8079d6464db64598b8",
+    "url": "/Hipdad.cool/static/js/runtime~main.936a2544.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Hipdad.cool/static/media/logo.5d5d9eef.svg"
+  }
+]);
